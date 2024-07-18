8c03e14f86865c4644f807f6e618533e114958af
graffuko <sarah.p.graff@edu.sait.ca>
2024-07-18 12:44:10 -0600

    week 4 complete

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,4 +1,73 @@
-"use Client";
+"use client";
 
 import { useState } from "react";
 
+export default function NewItem() {
+
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState("produce");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const item = {
+            name,
+            quantity,
+            category,
+        }
+
+        console.log(item);
+
+        alert(`You added ${quantity} ${name} item(s) in the ${category} category to the list!`);
+
+        setName("");
+        setQuantity(1);
+        setCategory("produce");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Name: </label>
+                <input 
+                    required type="text"  
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div>
+                <label>Quantity: </label>
+                <input 
+                    required 
+                    type="number" 
+                    min="1" 
+                    max="99" 
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)} 
+                />
+            </div>
+            <div>
+                <label>Category: </label>
+                <select
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    required
+                >
+                    <option value = "produce">Produce</option>
+                    <option value = "dairy">Dairy</option>
+                    <option value = "bakery">Bakery</option>
+                    <option value = "meat">Meat</option>
+                    <option value = "frozen foods">Frozen Foods</option>
+                    <option value = "canned goods">Canned Goods</option>
+                    <option value = "dry goods">Dry Goods</option>
+                    <option value = 'beverages'>Beverages</option>
+                    <option value = "snacks">Snacks</option>
+                    <option value = "household">Household</option>
+                    <option value = "other">Other</option>
+                </select>
+            </div>
+            <button type="submit">Add Item</button>
+        </form>
+    );
+}
